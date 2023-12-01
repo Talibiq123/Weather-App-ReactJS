963a4fb55ae1994cf792d50dc5ac3d918a6cf425
Talibiq123 <talibsaeed90@gmail.com>
2023-12-01 18:49:24 +0530

    kjvod

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './app.css';
 
 
 function App() {
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
+  // const date = new Date();
+  // let month = date.getMonth();
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
   const [hemisphere, setHemisphere] = useState("");
+  const [month, setMonth] = useState(new Date().getMonth());
+
+  
 
 
   function getLocation() {
@@ -26,6 +31,9 @@ function App() {
       console.log("Geolocation is not supported by this browser.");
     }
 
+
+
+
     
   }
 
@@ -34,10 +42,27 @@ function App() {
 
   return (
     <div>
-      <h1>Latitude: {latitude}</h1>
-      <h1>Longitude: {longitude}</h1>
-      <h1>Hemisphere: {hemisphere}</h1>
       <button onClick={getLocation}>get location</button>
+      {
+        (hemisphere === "Northern Hemisphere" && (month >= 2 && month <= 9)) || 
+        (hemisphere === "Southern Hemisphere" && (month < 2 || month > 9))? (
+          <div>
+            <p>{longitude} {latitude}</p>
+            <h1>It's Summer in the Northern Hemesphere.</h1>
+            <p>Aaya Mousam Thande Thande Dermi Cool Ka</p>
+            <img src="https://images.twinkl.co.uk/tw1n/image/private/t_630/u/ux/wheat-2391348-1920_ver_1.jpg" alt="summer" />
+          </div>
+        ) : (
+          hemisphere === "Northern Hemisphere" && (month <= 2 || month >= 9)? (
+          <div>
+            <h1>It's winter in the northern hemesphere.</h1>
+            <p>Winter is Here.</p>
+            <img src="https://www.usatoday.com/gcdn/presto/2023/03/03/USAT/3f908dae-00da-4d38-aa10-e82bbc92593b-AP_Winter_Weather_California.jpg" alt="winter" />
+          </div> ) : (
+            <h1>Relax We are fetching...</h1>
+          )
+        )
+      }
     </div>
   );
 }
